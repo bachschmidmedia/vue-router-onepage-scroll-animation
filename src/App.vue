d798672d01523cc91324ffb62401876291243be3
abachschmid <>
2021-12-08 13:52:19 +0100

    Scroll "prevent-areas" added

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,11 @@
       router-link(to="/page3") P3 
       router-link(to="/page4") P4 
       div {{ direction }}
-      div(style="max-height: 50px; overflow: scroll;")
-        p(v-for="i in Array(100)") JO
+      div(style="max-height: 50px; overflow: scroll; background-color: red;" @wheel="preventWheel" @touchmove="preventWheel")
+        p(v-for="i in Array(20)") PREVENT ALL
+
+      div(style="max-height: 50px; overflow: scroll; background-color: blue;" @scroll="preventWheel")
+        p(v-for="i in Array(20)") PREVENT SCROLL
     transition(
       :name="direction > 0 ? 'slideinup' : 'slideindown'"
       mode="out-in"
@@ -27,6 +30,7 @@ export default {
   data: function () {
     return {
       wheeling: null,
+      preventer: null,
       last_speed: 0,
       direction: 1,
 
@@ -35,7 +39,6 @@ export default {
   },
 
   mounted: function () {
-
     window.addEventListener("mousewheel", this.wheel);
     window.addEventListener("DOMMouseScroll", this.wheel);
     window.addEventListener("wheel", this.wheel);
@@ -44,21 +47,32 @@ export default {
     window.addEventListener("touchmove", this.touchMove);
   },
   methods: {
+    preventWheel() {
+      console.log("prevent wheel!");
+      this.router_locked = true;
 
-    touchStart (e) {
+      clearTimeout(this.preventer);
+      this.preventer = setTimeout(() => {
+        console.log("unlock router");
+        this.preventer = undefined;
+        this.router_locked = false;
+      }, 250);
+    },
+
+    touchStart(e) {
       this.ts = e.touches[0].clientY;
     },
 
-    touchMove (e) {
+    touchMove(e) {
       var te = e.changedTouches[0].clientY;
       if (this.ts > te + 5) {
-        const dir = 1
-        this.direction = dir
-        this.tryScrollTo(1)
+        const dir = 1;
+        this.direction = dir;
+        this.tryScrollTo(1);
       } else if (this.ts < te - 5) {
-        const dir = -1
-        this.direction = dir
-        this.tryScrollTo(-1)
+        const dir = -1;
+        this.direction = dir;
+        this.tryScrollTo(-1);
       }
     },
 
@@ -93,18 +107,20 @@ export default {
       }, 100);
     },
 
-    tryScrollTo (dir = this.direction) {
-      if (!this.router_locked) {
-        const currIndex = this.$router.options.routes.findIndex(
-          (x) => x.name === this.$route.name
-        );
-        const nextIndex = currIndex + dir;
-        const nextRoute = this.$router.options.routes[nextIndex] ?? false;
+    tryScrollTo(dir = this.direction) {
+      setTimeout(() => {
+        if (!this.router_locked) {
+          const currIndex = this.$router.options.routes.findIndex(
+            (x) => x.name === this.$route.name
+          );
+          const nextIndex = currIndex + dir;
+          const nextRoute = this.$router.options.routes[nextIndex] ?? false;
 
-        if (nextRoute) {
-          this.$router.push({ name: nextRoute.name });
+          if (nextRoute) {
+            this.$router.push({ name: nextRoute.name });
+          }
         }
-      }
+      }, 100);
     },
 
     onLeave() {
