350969e89860eb882e4031ebf1e735ca1b595f95
abachschmid <>
2021-12-08 14:06:42 +0100

    mixin created

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
       router-link(to="/page2") P2 
       router-link(to="/page3") P3 
       router-link(to="/page4") P4 
-      div {{ direction }}
+      div Direction: {{ direction }}
+
       div(style="max-height: 50px; overflow: scroll; background-color: red;" @wheel="preventWheel" @touchmove="preventWheel")
         p(v-for="i in Array(20)") PREVENT ALL
 
@@ -26,110 +27,13 @@
 </template>
 
 <script>
+import fullpagescroll from './mixins/fullpagescroll'
+
 export default {
-  data: function () {
-    return {
-      wheeling: null,
-      preventer: null,
-      last_speed: 0,
-      direction: 1,
 
-      pages: ["Page1", "Page2", "Page3", "Page4"],
-    };
-  },
+  mixins: [
+    fullpagescroll
+  ],
 
-  mounted: function () {
-    window.addEventListener("mousewheel", this.wheel);
-    window.addEventListener("DOMMouseScroll", this.wheel);
-    window.addEventListener("wheel", this.wheel);
-
-    window.addEventListener("touchstart", this.touchStart);
-    window.addEventListener("touchmove", this.touchMove);
-  },
-  methods: {
-    preventWheel() {
-      console.log("prevent wheel!");
-      this.router_locked = true;
-
-      clearTimeout(this.preventer);
-      this.preventer = setTimeout(() => {
-        console.log("unlock router");
-        this.preventer = undefined;
-        this.router_locked = false;
-      }, 250);
-    },
-
-    touchStart(e) {
-      this.ts = e.touches[0].clientY;
-    },
-
-    touchMove(e) {
-      var te = e.changedTouches[0].clientY;
-      if (this.ts > te + 5) {
-        const dir = 1;
-        this.direction = dir;
-        this.tryScrollTo(1);
-      } else if (this.ts < te - 5) {
-        const dir = -1;
-        this.direction = dir;
-        this.tryScrollTo(-1);
-      }
-    },
-
-    wheel(event) {
-      // if(this.router_locked) return
-
-      let speed;
-      if (event.wheelDelta) {
-        if ((event.wheelDelta % 120) - 0 == 0) {
-          speed = event.wheelDelta / 120;
-        } else {
-          speed = event.wheelDelta / 12;
-        }
-      } else {
-        let rawAmmount = event.deltaY ? event.deltaY : event.detail;
-        speed = -(rawAmmount % 3 ? rawAmmount * 10 : rawAmmount / 3);
-      }
-
-      if (Math.abs(speed) > 1) {
-        if (Math.abs(speed) > Math.abs(this.last_speed) + 2) {
-          this.last_speed = speed;
-          console.log("scroll to", speed);
-          this.direction = speed > 0 ? -1 : 1;
-          this.tryScrollTo(this.direction);
-        }
-      }
-
-      clearTimeout(this.wheeling);
-      this.wheeling = setTimeout(() => {
-        this.wheeling = undefined;
-        this.last_speed = 0;
-      }, 100);
-    },
-
-    tryScrollTo(dir = this.direction) {
-      setTimeout(() => {
-        if (!this.router_locked) {
-          const currIndex = this.$router.options.routes.findIndex(
-            (x) => x.name === this.$route.name
-          );
-          const nextIndex = currIndex + dir;
-          const nextRoute = this.$router.options.routes[nextIndex] ?? false;
-
-          if (nextRoute) {
-            this.$router.push({ name: nextRoute.name });
-          }
-        }
-      }, 100);
-    },
-
-    onLeave() {
-      this.router_locked = true;
-    },
-
-    afterLeave() {
-      this.router_locked = false;
-    },
-  },
 };
 </script>
---
+++ src/mixins/fullpagescroll.js
@@ -0,0 +1,110 @@
+export default {
+  data: function () {
+    return {
+      wheeling: null,
+      preventer: null,
+      last_speed: 0,
+      direction: 1,
+    };
+  },
+
+  mounted: function () {
+    window.addEventListener("mousewheel", this.wheel);
+    window.addEventListener("DOMMouseScroll", this.wheel);
+    window.addEventListener("wheel", this.wheel);
+
+    window.addEventListener("touchstart", this.touchStart);
+    window.addEventListener("touchmove", this.touchMove);
+  },
+  methods: {
+    lockRouter() {
+      this.router_locked = true;
+    },
+
+    unlockRouter() {
+      this.router_locked = false;
+    },
+
+    preventWheel() {
+      this.lockRouter();
+
+      clearTimeout(this.preventer);
+      this.preventer = setTimeout(() => {
+        this.preventer = undefined;
+        this.last_speed = 0;
+        this.unlockRouter();
+      }, 250);
+    },
+
+    touchStart(e) {
+      this.ts = e.touches[0].clientY;
+    },
+
+    touchMove(e) {
+      var te = e.changedTouches[0].clientY;
+      if (this.ts > te + 5) {
+        const dir = 1;
+        this.direction = dir;
+        this.tryScrollTo(1);
+      } else if (this.ts < te - 5) {
+        const dir = -1;
+        this.direction = dir;
+        this.tryScrollTo(-1);
+      }
+    },
+
+    wheel(event) {
+      // if(this.router_locked) return
+      let speed;
+      if (event.wheelDelta) {
+        if ((event.wheelDelta % 120) - 0 == 0) {
+          speed = event.wheelDelta / 120;
+        } else {
+          speed = event.wheelDelta / 12;
+        }
+      } else {
+        let rawAmmount = event.deltaY ? event.deltaY : event.detail;
+        speed = -(rawAmmount % 3 ? rawAmmount * 10 : rawAmmount / 3);
+      }
+
+      if (
+        Math.abs(speed) > 1 &&
+        Math.abs(speed) > Math.abs(this.last_speed) + 2
+      ) {
+        this.last_speed = speed;
+        this.direction = speed > 0 ? -1 : 1;
+        this.tryScrollTo(this.direction);
+      }
+
+      clearTimeout(this.wheeling);
+      this.wheeling = setTimeout(() => {
+        this.wheeling = undefined;
+        this.last_speed = 0;
+      }, 100);
+    },
+
+    tryScrollTo(dir = this.direction) {
+      setTimeout(() => {
+        if (!this.router_locked) {
+          const currIndex = this.$router.options.routes.findIndex(
+            (x) => x.name === this.$route.name
+          );
+          const nextIndex = currIndex + dir;
+          const nextRoute = this.$router.options.routes[nextIndex] ?? false;
+
+          if (nextRoute) {
+            this.$router.push({ name: nextRoute.name });
+          }
+        }
+      }, 100);
+    },
+
+    onLeave() {
+      this.lockRouter();
+    },
+
+    afterLeave() {
+      this.unlockRouter();
+    },
+  },
+};
